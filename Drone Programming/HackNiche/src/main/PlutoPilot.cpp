7432a651c17fb1a4bebbb7805488d90532117878
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-02-17 16:48:53 +0530

    Chuck to arm program added

--- Drone Programming/HackNiche/src/main/PlutoPilot.cpp
+++ Drone Programming/HackNiche/src/main/PlutoPilot.cpp
@@ -1,67 +1,41 @@
-// Do not remove the include below
+/*Do not remove the include below*/
 #include "PlutoPilot.h"
-#include "Peripheral.h"
-#include "Utils.h"
-#include "Math.h"
-#include "User.h"
 #include "Sensor.h"
-#include "Control.h"
-#include "Estimate.h"
+#include "User.h"
+#include "Utils.h"
 
-//The setup function is called once at PlutoX's hardware startup
-void plutoInit()
+/*The setup function is called once at Pluto's hardware startup*/
+
+void plutoInit ()
 {
-	//Add your hardware initialization code here
-	Monitor.println("plutoInit ");
-	setUserLoopFrequency(50);
+	/*Add your hardware initialization code here*/
 }
 
-
-
-
-//The function is called once before plutoLoop() when you activate developer mode
-void onLoopStart()
+/*The function is called once before plutoLoop when you activate Developer
+Mode*/
+void onLoopStart ()
 {
-	//Do your one time stuff here
-	LED.flightStatus(DEACTIVATE);
-	GPIO.init(Pin8, INPUT);
-	Monitor.println("onLoopStart ");
+	/*do your one time tasks here*/
+	LED.flightStatus(DEACTIVATE); /*Disable default Led behavior*/
 }
 
-
-
-//The loop function is called in an endless loop
-void plutoLoop()
+/*The loop function is called in an endless loop*/
+void plutoLoop ()
 {
-	Monitor.println(" ",GPIO.read(Pin8));
-	//Add your repeated code here
-
-	if(GPIO.read(Pin8) == 0){
-
-	//	Command.arm();
-		LED.set(RED, ON);
-		LED.set(GREEN, OFF);
-	//	LED.set(BLUE, OFF);
+	/*Add your repeated code here*/
+	if (Acceleration.getNetAcc()< 2 &&(!FlightStatus.check(FS_CRASHED)))
+	/*Condition for free fall*/
+	{
+		Command.arm(); /*Arm the drone*/
+		LED. set (RED, ON);
+		LED. set (GREEN, ON);
 	}
-	else{
-	//	Command.arm();
-		LED.set(RED, OFF);
-		LED.set(GREEN, ON);
-	//	LED.set(BLUE, OFF);
-	}
-
 }
 
-
-
-//The function is called once after plutoLoop() when you deactivate developer mode
-void onLoopFinish()
+/*The function is called once after plutoLoop when you deactivate Developer
+Mode*/
+void onLoopFinish ()
 {
-//Do your cleanup stuff here
-
-
+	/*do your cleanup tasks here*/
+	LED.flightStatus(ACTIVATE); /*Enable the default LED behavior*/
 }
-
-
-
-
