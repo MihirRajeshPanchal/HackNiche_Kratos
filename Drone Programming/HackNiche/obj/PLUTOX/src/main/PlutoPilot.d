cb40246070ffcaf3a863bd3a71b27ebaed7dda55
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-02-17 15:00:03 +0530

    Added led status + proximity sensor

---
+++ Drone Programming/HackNiche/obj/PLUTOX/src/main/PlutoPilot.d
@@ -0,0 +1,22 @@
+obj/PLUTOX/src/main/PlutoPilot.o: src/main/PlutoPilot.cpp \
+ src/includes/PlutoPilot.h platform/Peripheral.h platform/Specifiers.h \
+ platform/Utils.h platform/User.h platform/Sensor.h platform/Comman.h \
+ platform/Control.h platform/Estimate.h
+
+src/includes/PlutoPilot.h:
+
+platform/Peripheral.h:
+
+platform/Specifiers.h:
+
+platform/Utils.h:
+
+platform/User.h:
+
+platform/Sensor.h:
+
+platform/Comman.h:
+
+platform/Control.h:
+
+platform/Estimate.h:
